85cb43d47f3d2615a35cfa52fc71fb44da499a93
As7Vezes <denysn60@gmail.com>
2023-10-24 14:46:17 -0300

    feature/adding Apollo server with GrahpQL

---
+++ dist/index.js
@@ -0,0 +1,26 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+const typeDefs = `
+
+    type hello {
+        hello: String
+    }
+
+    type Query {
+        hello: String
+    }
+
+`;
+const resolvers = {
+    Query: {
+        hello: () => "Hello Mundão!"
+    }
+};
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 }
+});
+console.log('Server is Running!');
